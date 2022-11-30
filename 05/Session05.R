cb23e49ada0718ab7e395eb97275ddd096eba46e
devRCR <renzo.chan@outlook.com>
2022-11-30 13:52:13 -0500

    05 changed

--- 05/Session05.R
+++ 05/Session05.R
@@ -55,3 +55,63 @@ attach(datos)
 (consulta<-tail(datos))
 (consulta<-head(datos,10))
 (consulta<-tail(datos,15))
+
+(consultas<-head(select(datos,c(Segmento,Empleado,Departamento,VentasBrutas)),6))
+
+(consultas<-tail(select(datos,c(5:12)),10))
+
+# distinct: cuantos valores distintos existen
+distinct(datos,Segmento)
+distinct(datos,Departamento)
+distinct(datos,Empleado)
+
+(consulta<-distinct(datos,Departamento,Distrito))
+
+# filtrar ==, !=, >, <, >=, <=
+(consulta<-filter(datos,Departamento=="Lima"))
+(consulta<-filter(datos,Departamento!="Lima"))
+
+(consulta<-filter(datos,UnidadesVendidas>2000))
+
+# y ---> & o   ----->
+(consulta<-filter(datos,Departamento=="Lima" & UnidadesVendidas>2000))
+(consulta<-filter(datos,Departamento=="Lima" | UnidadesVendidas>2000))
+
+(consulta<-filter(datos,Departamento=="Lima" | Departamento=="Arequipa" | 
+                    (UnidadesVendidas>=2000 & UnidadesVendidas<=3000)))
+
+# %in%  -------> sirve para agregar lista de las filas de una columna
+(consulta<-filter(datos,Departamento %in% c("Lima","Arequipa") | 
+                  (UnidadesVendidas>=2000 & UnidadesVendidas<=3000)))
+
+# between -------> retorna rango de numero
+(consulta<-filter(datos,Departamento %in% c("Lima","Arequipa") | 
+                    between(UnidadesVendidas,2000,3000))) 
+
+(consulta<-filter(datos,!Departamento %in% c("Lima","Arequipa") | 
+                    between(UnidadesVendidas,2000,3000))) 
+
+# agregar columna
+(consulta<-mutate(datos,Impuesto=VentasBrutas*0.18))
+
+(consulta<-mutate(datos,Zona=case_when(
+  Departamento=="Lima"~"centro",
+  Departamento=="La Libertad"~"Norte",
+  Departamento=="Arequipa"~"Sur",
+  TRUE~"Ninguno"
+)))
+
+# ordenar columanas 
+(consulta<-arrange(datos,Segmento))
+(consulta<-arrange(datos,Departamento))
+(consulta<-arrange(datos,desc(Departamento)))
+
+(consulta<-arrange(datos,desc(Departamento),desc(Distrito)))
+
+# ctrl + shift + m  %>%
+(consulta<-select(datos,Segmento,Empleado,Departamento,UnidadesVendidas) %>% 
+    filter(Departamento %in% c("Arequipa","Lima")))
+
+(consulta<-select(datos,Segmento,Empleado,Departamento,UnidadesVendidas) %>% 
+    filter(Departamento %in% c("Arequipa","Lima")) %>% 
+    arrange(desc(UnidadesVendidas)))
